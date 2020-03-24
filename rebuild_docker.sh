e9b02cf8412b90aebbd6df68c85cc050ef14080a
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-03-25 00:44:09 +0100

    Make removal of the orkadistribution files optional; this is the only sane default behaviour anyways

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
-rm -rf orkaevolution
+HARD_RESET=""
 
-git clone git@i2git.cs.fau.de:orka/s2scompiler/orkaevolution.git
-pushd orkaevolution
-git submodule sync
-git submodule update --init --recursive
-popd
+# rm -rf orkaevolution
+while [ "${1:-}" != "" ]; do
+    case "${1}" in
+        "--reset-hard" | "--hard-reset")
+            shift
+            HARD_RESET=true
+            ;;
+        *)
+            shift
+            ;;
+    esac
+    shift
+done
+
+
+[ "$HARD_RESET" == "true" ] && {
+    echo rm -rf orkaevolution
+
+    git clone git@i2git.cs.fau.de:orka/s2scompiler/orkaevolution.git
+    pushd orkaevolution
+    git submodule sync
+    git submodule update --init --recursive
+    popd
+}
+
 
 VIVADO_VERSION="${VIVADO_VERSION:-2018.2}"
 
