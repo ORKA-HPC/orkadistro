33ccb1dbf944a1f5a1dbbf4f1d8ec44fc358b4f9
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-03-25 00:44:58 +0100

    Add overlay mount (first draft) in order to make installation of additional board files in the VM as user transparent as possible

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@ dockerTag=rose
 dockerName=rose
 
 XILINX_HOST_PATH="${XILINX_HOST_PATH:-"/opt/Xilinx"}"
+XILINX_VIVADO_VERSION="${XILINX_VIVADO_VERSION:-"2018.2"}"
 XILINX_DOCKER_PATH="${XILINX_DOCKER_PATH:-"/usr/Xilinx"}"
 
-[ ! -d $XILINX_HOST_PATH ] && { 
+[ ! -d $XILINX_HOST_PATH ] && {
 	echo $XILINX_HOST_PATH does not exist in host file system
 	exit 1
 }
@@ -14,12 +15,20 @@ XILINX_DOCKER_PATH="${XILINX_DOCKER_PATH:-"/usr/Xilinx"}"
 sudo docker stop $dockerName
 sudo docker rm $dockerName
 
+upper_dir="./__vivado_boardfiles_overlay_upper_dir"
+work_dir="./__vivado_boardfiles_overlay_work_dir"
+mnt_point="./__vivado_boardfiles_overlay_mnt_point"
+lower_dir="${XILINX_HOST_PATH}/Vivado/${XILINX_VIVADO_VERSION}/data/boards/board_files/"
+
+sudo mount -t overlay overlay \
+     -o lowerdir="$lower_dir",upperdir="$upper_dir",workdir="$work_dir" \
+     "$mnt_point"
+
 sudo docker run \
 	--name $dockerName -t -d \
 	-v $PWD:/mnt \
 	-v $PWD/orkaevolution:/home/build/orkaevolution \
 	-v $XILINX_HOST_PATH:/$XILINX_DOCKER_PATH \
 	-v $PWD/fpgainfrastructure:/home/build/fpgainfrastructure \
-	-v $PWD/ranger:/home/build/.config/ranger \
+        -v $PWD/vivado-boards:/home/build \
 	$dockerTag
-
