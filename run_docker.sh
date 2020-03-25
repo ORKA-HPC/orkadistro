0458f289312375f916d2942f680d7c635319cee7
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-03-25 01:09:44 +0100

    Create overlay mount for file system union of the following directory in xilinx $XIL_PREFIX/Vivado/$VIVADO_VERS/data/boards/board-files; The mountpoint introduces a union of standard pre-installed board files from the vivado system and all board files for diligent boards (fpgainfrastructure needs them to be installed at that location).

--- run_docker.sh
+++ run_docker.sh
@@ -15,19 +15,28 @@ XILINX_DOCKER_PATH="${XILINX_DOCKER_PATH:-"/usr/Xilinx"}"
 sudo docker stop $dockerName
 sudo docker rm $dockerName
 
-upper_dir="./__vivado_boardfiles_overlay_upper_dir"
+mkdir -p \
+      __vivado_boardfiles_overlay_work_dir \
+      __vivado_boardfiles_overlay_mnt_point
+
+vivado_board_files_dir="vivado-boards/new/board_files/"
+
+upper_dir="./$vivado_board_files_dir"
 work_dir="./__vivado_boardfiles_overlay_work_dir"
 mnt_point="./__vivado_boardfiles_overlay_mnt_point"
 lower_dir="${XILINX_HOST_PATH}/Vivado/${XILINX_VIVADO_VERSION}/data/boards/board_files/"
 
+sudo umount -q "$mnt_point"
 sudo mount -t overlay overlay \
      -o lowerdir="$lower_dir",upperdir="$upper_dir",workdir="$work_dir" \
      "$mnt_point"
 
+docker_mnt_point="${XILINX_DOCKER_PATH}/Vivado/${XILINX_VIVADO_VERSION}/data/boards/board_files/"
 sudo docker run \
 	--name $dockerName -t -d \
 	-v $PWD:/mnt \
 	-v $PWD/orkaevolution:/home/build/orkaevolution \
 	-v $XILINX_HOST_PATH:/$XILINX_DOCKER_PATH \
 	-v $PWD/fpgainfrastructure:/home/build/fpgainfrastructure \
+        -v $PWD/"$mnt_point":"$docker_mnt_point" \
 	$dockerTag
