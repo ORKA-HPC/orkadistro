fa78557978d689e049637c62f9286648179a0a67
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-04-07 21:08:07 +0200

    major refactor of run script

--- run_docker.sh
+++ run_docker.sh
@@ -12,38 +12,6 @@ XILINX_VIVADO_VERSION="${XILINX_VIVADO_VERSION:-"2018.2"}"
     exit 1
 }
 
-## cli "parsing"
-exec_into_container="false"
-stop_and_unmount="false"
-
-while [ "${1:-}" != "" ]; do
-    case "$1" in
-        "--stop-and-unmount" | "-q")
-            stop_and_unmount="true"
-            ;;
-        "--exec-shell" | "-e")
-            exec_into_container="true"
-            ;;
-        *)
-            shift
-            ;;
-    esac
-    shift
-done
-
-if [ "${exec_into_container}" == "true" ]; then
-    docker exec -it $DOCKER_NAME bash -l
-    exit 0
-fi
-
-## docker foo wohoo
-sudo docker stop $DOCKER_NAME
-sudo docker rm $DOCKER_NAME
-
-mkdir -p \
-      __vivado_boardfiles_overlay_work_dir \
-      __vivado_boardfiles_overlay_mnt_point
-
 vivado_board_files_dir="vivado-boards/new/board_files/"
 
 upper_dir="./$vivado_board_files_dir"
@@ -51,26 +19,82 @@ work_dir="./__vivado_boardfiles_overlay_work_dir"
 mnt_point="./__vivado_boardfiles_overlay_mnt_point"
 lower_dir="${XILINX_HOST_PATH}/Vivado/${XILINX_VIVADO_VERSION}/data/boards/board_files/"
 
-if [ "${stop_and_unmount}" == "true" ]; then
-    sudo umount -q "$mnt_point"
-    # we delete this so that ./rebuild_docker.sh can
-    # work again.
-    rm -rf "$work_dir"
-    exit 0
-fi
-
-sudo umount -q "$mnt_point"
-sudo mount -t overlay overlay \
-     -o lowerdir="$lower_dir",upperdir="$upper_dir",workdir="$work_dir" \
-     "$mnt_point"
-
 docker_mnt_point="${XILINX_DOCKER_PATH}/Vivado/${XILINX_VIVADO_VERSION}/data/boards/board_files/"
-sudo docker run \
-     --name $DOCKER_NAME -t -d \
-     -v $PWD:/mnt \
-     -v $PWD/orkaevolution:/home/build/orkaevolution \
-     -v $XILINX_HOST_PATH:/$XILINX_DOCKER_PATH \
-     -v $PWD/fpgainfrastructure:/home/build/fpgainfrastructure \
-     -v $PWD/roserebuild:/home/build/roserebuild \
-     -v $PWD/"$mnt_point":"$docker_mnt_point" \
-     $DOCKER_TAG
+
+## cli "parsing"
+exec_into_container="false"
+stop_and_unmount="false"
+run_in_background="false"
+stop_remove_and_unmount="false"
+
+while [ "${1:-}" != "" ]; do
+    case "$1" in
+        "--stop-and-unmount" | "-q")
+            stop_and_unmount="true"
+            ;;
+        "--stop-remove-and-unmount" | "-q")
+            stop_remove_and_unmount="true"
+            ;;
+        "--exec-shell" | "-e")
+            exec_into_container="true"
+            ;;
+        "--run-background" | "-r")
+            run_in_background="true"
+            ;;
+        *)
+            echo [unknown cli flag]
+            shift
+            ;;
+    esac
+    shift
+done
+
+function setup_board_files_overlay_mount() {
+    mkdir -p \
+          __vivado_boardfiles_overlay_work_dir \
+          __vivado_boardfiles_overlay_mnt_point
+
+    sudo umount -q "$mnt_point"
+    sudo mount -t overlay overlay \
+         -o lowerdir="$lower_dir",upperdir="$upper_dir",workdir="$work_dir" \
+         "$mnt_point"
+}
+
+function unmount_boardfiles_overlay() {
+    sudo umount -q "$mnt_point"
+    # we delete this so that ./rebuild_docker.sh can work again.
+    rm -rf "$work_dir"
+}
+
+function launch_container_background() {
+    sudo docker run \
+         --name $DOCKER_NAME -t -d \
+         -v $PWD:/mnt \
+         -v $PWD/orkaevolution:/home/build/orkaevolution \
+         -v $XILINX_HOST_PATH:/$XILINX_DOCKER_PATH \
+         -v $PWD/fpgainfrastructure:/home/build/fpgainfrastructure \
+         -v $PWD/roserebuild:/home/build/roserebuild \
+         -v $PWD/"$mnt_point":"$docker_mnt_point" \
+         $DOCKER_TAG
+}
+
+[ "${stop_and_unmount}" == "true" ] && {
+    sudo docker stop $DOCKER_NAME
+    unmount_boardfiles_overlay
+}
+
+[ "${stop_remove_and_unmount}" == "true" ] && {
+    sudo docker stop $DOCKER_NAME
+    sudo docker rm $DOCKER_NAME
+    unmount_boardfiles_overlay
+}
+
+[ "${run_in_background}" == "true" ] && {
+    setup_board_files_overlay_mount
+    launch_container_background
+}
+
+[ "${exec_into_container}" == "true" ] && {
+    docker exec -it $DOCKER_NAME bash -l || \
+        echo [could not open shell in container, probably you need to start it first. exit]
+}
