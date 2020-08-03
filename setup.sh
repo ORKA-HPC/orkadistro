1071e853022119829d0af618cca2030127e6e96d
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-08-03 20:02:47 +0200

    Refactor

--- setup.sh
+++ setup.sh
@@ -4,11 +4,10 @@ git submodule sync --recursive
 git submodule update --init --recursive
 
 ./rebuild_docker.sh
-./run_docker.sh -r
 
 (cd roserebuild && ./rebuild.sh --prepare --with-edg-repo)
 
-./run_docker.sh --exec-non-interactive \
+./run_docker.sh -r -q --exec-non-interactive \
 	bash -c "cd roserebuild; ./rebuild.sh -b -i"
-./run_docker.sh --exec-non-interactive \
+./run_docker.sh -r -q --exec-non-interactive \
 	bash -c "cd orkaevolution; cmake . ; make -j"
