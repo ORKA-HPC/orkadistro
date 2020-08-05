a16072d152cfa2e392cced6f836284ff6ee36ab0
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-08-05 14:40:39 +0200

    Source /etc/profile.d/ccache.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ echo [build docker image]
 ./rebuild_docker.sh
 
 echo [prepare rose]
-(cd roserebuild && ./rebuild.sh --prepare --with-edg-repo)
+( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
 
 echo [build rose]
 ./run_docker.sh -r -q --exec-non-interactive \
-	bash -c "cd roserebuild; ./rebuild.sh -b -i"
+	bash -l -c "cd roserebuild; ./rebuild.sh -b -i"
 
 echo [build orkaevolution
 ./run_docker.sh -r -q --exec-non-interactive \
-	bash -c "cd orkaevolution; cmake . ; make -j"
+	bash -l -c "cd orkaevolution; cmake . ; make -j"
