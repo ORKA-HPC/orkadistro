f2051bcd944e6f0458b7e61be3d1b6bd51c0ae06
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-08-05 14:13:21 +0200

    Be a little more verbose

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+echo [submodule setup]
 git submodule sync --recursive
 git submodule update --init --recursive
 
+echo [build docker image]
 ./rebuild_docker.sh
 
+echo [prepare rose]
 (cd roserebuild && ./rebuild.sh --prepare --with-edg-repo)
 
+echo [build rose]
 ./run_docker.sh -r -q --exec-non-interactive \
 	bash -c "cd roserebuild; ./rebuild.sh -b -i"
+
+echo [build orkaevolution
 ./run_docker.sh -r -q --exec-non-interactive \
 	bash -c "cd orkaevolution; cmake . ; make -j"
