9815c442ce2bdbe82e158d8aac5cf24c3dbc9336
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-09-09 01:52:04 +0200

    Test setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ echo [submodule setup]
 git submodule sync --recursive
 git submodule update --init --recursive
 
-# echo [build docker image]
-# ./rebuild_docker.sh
-# 
-# echo [prepare rose]
-# ( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
-# 
-# echo [build rose]
-# ./run_docker.sh -r -q --exec-non-interactive \
-# 	bash -l -c "cd roserebuild; ./rebuild.sh -b -i"
-# 
-# echo [build orkaevolution]
-# ./run_docker.sh -r -q --exec-non-interactive \
-# 	bash -l -c "cd orkaevolution; cmake . ; make -j"
+echo [build docker image]
+./rebuild_docker.sh
+
+echo [prepare rose]
+( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
+
+echo [build rose]
+./run_docker.sh -r -q --exec-non-interactive \
+	bash -l -c "cd roserebuild; ./rebuild.sh --clean -b -i"
+
+echo [build orkaevolution]
+./run_docker.sh -r -q --exec-non-interactive \
+	bash -l -c "cd orkaevolution; cmake . ; make clean ; make -j"
 
 echo [build tapasco]
 ./run_docker.sh -r -q --exec-non-interactive \
