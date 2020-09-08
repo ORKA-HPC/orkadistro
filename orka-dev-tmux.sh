cf32b9f0f883528768467ec66b4095be0ebb7c6a
Florian Mayer <florian.andrefranc.mayer@fau.de>
2020-09-09 00:33:06 +0200

    parameterize

--- orka-dev-tmux.sh
+++ orka-dev-tmux.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+distro="A_orkadistro"
+
 tmux \
 	new-session \; \
-	send-keys 'cd && cd A_orkadistro && ./run_docker.sh -e' C-m \; \
+	send-keys "cd && cd $distro && ./run_docker.sh -e" C-m \; \
 	send-keys 'cd && cd orkaevolution' C-m \; \
 	split-window -h \; \
-	send-keys 'cd && cd A_orkadistro/orkaevolution && sl-oe orka_evolution.cpp' C-m \; \
+	send-keys "cd && cd $distro/orkaevolution && sl-oe orka_evolution.cpp" C-m \; \
 	neww \; \
-	send-keys 'cd && cd A_orkadistro && ./run_docker.sh -e' C-m \; \
+	send-keys "cd && cd $distro && ./run_docker.sh -e" C-m \; \
 	send-keys 'cd && cd roserebuild' C-m \; \
 	split-window -h \; \
-	send-keys 'cd && cd A_orkadistro/roserebuild/rose_repo/src' C-m \;
+	send-keys "cd && cd $distro/roserebuild/rose_repo/src" C-m \;
