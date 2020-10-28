afdf154a6122743a25c5f3d55955f7bdb5af8fef
Florian Mayer <mayer.florian@web.de>
2020-10-28 17:01:48 +0100

    Add clean mode

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+CLEAN_BUILD_ROSE=0
+
+while [ "${1:-}" != "" ]; do
+	case "$1" in
+		"--clean-build-rose" | "-c")
+			CLEAN_BUILD_ROSE=1
+			shift
+			;;
+
+		*)
+			;;
+	esac
+done
+
 echo [submodule setup]
 git submodule sync --recursive
 git submodule update --init --recursive
@@ -7,12 +21,19 @@ git submodule update --init --recursive
 echo [build docker image]
 ./rebuild_docker.sh
 
-echo [prepare rose]
-( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
+if [ "$CLEAN_BUILD_ROSE" = 1 ]; then
+    echo [prepare rose]
+    ( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
+
+
+    echo [build rose]
+    ./run_docker.sh -r -q --exec-non-interactive \
+	            bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh --clean -b -i"
+fi
 
 echo [build rose]
 ./run_docker.sh -r -q --exec-non-interactive \
-	bash -l -c "cd roserebuild; ./rebuild.sh --clean -b -i"
+	bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh -i"
 
 echo [build orkaevolution]
 ./run_docker.sh -r -q --exec-non-interactive \
@@ -29,8 +50,8 @@ echo [build tapasco]
                     . tapasco-setup.sh && tapasco-build-toolflow'
 
 ./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace && 
-	            . tapasco-setup.sh && cd ../tapasco/runtime && 
+        bash -l -c 'cd && cd tapasco-workspace &&
+	            . tapasco-setup.sh && cd ../tapasco/runtime &&
 		    { cmake . && make -j$MAX_CORES; }'
 
 ./run_docker.sh -r -q --exec-non-interactive \
@@ -39,5 +60,5 @@ echo [build tapasco]
                     cd ../tapasco/runtime && make install"'
 
 ./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace && 
+        bash -l -c 'cd && cd tapasco-workspace &&
 	            sudo cp tapasco-setup.sh /etc/profile.d/tapasco.sh'
