126df44e3d8b3156a4595bd77471ce97420bacc2
Florian Mayer <mayer.florian@web.de>
2020-10-30 10:37:01 +0100

    Make setup configurable

--- setup.sh
+++ setup.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 
 CLEAN_BUILD_ROSE=0
+CLEAN_BUILD_ORKA=0
+CLEAN_BUILD_TAPASCO=0
+INSTALL_ROSE=0
 
 while [ "${1:-}" != "" ]; do
-	case "$1" in
-		"--clean-build-rose" | "-c")
-			CLEAN_BUILD_ROSE=1
-			shift
-			;;
-
-		*)
-			;;
-	esac
+    case "$1" in
+        "--clean-build-rose" | "-c")
+            CLEAN_BUILD_ROSE=1
+            ;;
+        "--install-rose" | "-i")
+            INSTALL_ROSE=1
+            ;;
+        "--clean-build-tapasco")
+            CLEAN_BUILD_TAPASCO=1
+            ;;
+        "--clean-build-orka")
+            CLEAN_BUILD_ORKA=1
+            ;;
+        "--init")
+            CLEAN_BUILD_ROSE=1
+            CLEAN_BUILD_TAPASCO=1
+            CLEAN_BUILD_ORKA=1
+            INSTALL_ROSE=1
+            ;;
+        "--help" | "-h")
+            echo "--clean-build-rose"
+            exit 0
+            ;;
+        *)
+            ;;
+    esac
+    shift
 done
 
 echo [submodule setup]
@@ -25,41 +46,46 @@ if [ "$CLEAN_BUILD_ROSE" = 1 ]; then
     echo [prepare rose]
     ( cd roserebuild && ./rebuild.sh --prepare --with-edg-repo )
 
-
-
     echo [build rose]
     ./run_docker.sh -r -q --exec-non-interactive \
-	            bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh --clean -b -i"
+                    bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh --clean -b"
 fi
 
-echo [build rose]
-./run_docker.sh -r -q --exec-non-interactive \
-	bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh -i"
+if [ "$INSTALL_ROSE" = 1 ]; then
+    echo [install rose]
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c "cd roserebuild; MAX_CORES=4 ./rebuild.sh -i"
+fi
 
-echo [build orkaevolution]
-./run_docker.sh -r -q --exec-non-interactive \
-	bash -l -c "cd orkaevolution; cmake . ; make clean ; make -j"
+if [ "$CLEAN_BUILD_ORKA" = 1 ]; then
+    echo [build orkaevolution]
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c "cd orkaevolution; cmake . ; make clean ; make -j"
+fi
 
-echo [build tapasco]
-./run_docker.sh -r -q --exec-non-interactive \
-	bash -l -c 'cd && mkdir -p tapasco-workspace &&
-		    cd tapasco-workspace &&
-		    ../tapasco/tapasco-init.sh'
 
-./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace &&
+if [ "$CLEAN_BUILD_TAPASCO" = 1 ]; then
+    echo [build tapasco]
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c 'cd && mkdir -p tapasco-workspace &&
+                         cd tapasco-workspace &&
+                         ../tapasco/tapasco-init.sh'
+
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c 'cd && cd tapasco-workspace &&
                     . tapasco-setup.sh && tapasco-build-toolflow'
 
-./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace &&
-	            . tapasco-setup.sh && cd ../tapasco/runtime &&
-		    { cmake . && make -j$MAX_CORES; }'
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c 'cd && cd tapasco-workspace &&
+                    . tapasco-setup.sh && cd ../tapasco/runtime &&
+                    { cmake . && make -j$MAX_CORES; }'
 
-./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace &&
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c 'cd && cd tapasco-workspace &&
                     sudo bash -c ". tapasco-setup.sh &&
                     cd ../tapasco/runtime && make install"'
 
-./run_docker.sh -r -q --exec-non-interactive \
-        bash -l -c 'cd && cd tapasco-workspace &&
-	            sudo cp tapasco-setup.sh /etc/profile.d/tapasco.sh'
+    ./run_docker.sh -r -q --exec-non-interactive \
+                    bash -l -c 'cd && cd tapasco-workspace &&
+                    sudo cp tapasco-setup.sh /etc/profile.d/tapasco.sh'
+fi
