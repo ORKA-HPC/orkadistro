d1acbddc756e2fa871f5ad1ba207297a21ccf59f
Florian Mayer <mayer.florian@web.de>
2021-03-05 18:44:26 +0100

    Add config files

---
+++ cfg_files/profile_d_fpgainfrastructure_paths.sh
@@ -0,0 +1,3 @@
+export PATH="/home/build/fpgainfrastructure/hw/build_ip:$PATH"
+export PATH="/home/build/fpgainfrastructure/hw/orka_hw_configurator:$PATH"
+export PATH="/home/build/fpgainfrastructure/hw/:$PATH"
