0fd06cd7bef7e5fe66f06c541e089678b1458a69
Florian Mayer <mayer.florian@web.de>
2021-09-23 13:51:49 +0200

    Add CONTAINER_NAME

--- common_docker.sh
+++ common_docker.sh
@@ -5,3 +5,8 @@ IMAGE_NAME="${IMAGE_NAME:-"orkadistro-img-$(git rev-parse HEAD)"}"
 IMAGE_FULL_NAME="$IMAGE_NAME:$IMAGE_TAG"
 IMAGE_FULL_BUILD_NAME="$DOCKER_PUSH_PATH/$IMAGE_FULL_NAME"
 TARGET="${TARGET:-development}"
+CONTAINER_NAME="${CONTAINER_NAME:-"orkadistro-cont-$(sha256sum <(realpath $PWD) | cut -c 1-8)"}"
+
+function fullContName() {
+    echo "$CONTAINER_NAME-$TARGET"
+}
